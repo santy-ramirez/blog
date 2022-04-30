d47e6e44f348ec6ed3edce4442afe2af94c9d04c
santiago <santysjr956@gmail.com>
2022-04-30 12:37:26 -0300

    Deploy website - based on b0b4d602d1d836589e94dd56475f6d0628cd5a2d

---
+++ assets/js/814f3328.7c7b75ca.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[535],{5641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"segundo","permalink":"/blog/segundo"},{"title":"index","permalink":"/blog/index"}]}')}}]);
