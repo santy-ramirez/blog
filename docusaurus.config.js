ebd3d0229586ca3a239e0e0b2552cb191ad57095
santiago <santysjr956@gmail.com>
2022-05-03 15:48:21 -0300

    best navbar responsive

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
         },
         items: [
           
-          {to: '/blog', label: 'Blog', position: 'left'},
+        
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
---
+++ src/components/Avatar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+
+export default function Avatar() {
+  return (
+    <div>
+      <img src="" alt="">
+      <h1>hola muno</h1>
+    </div> 
+  );
+}
